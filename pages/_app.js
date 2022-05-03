e7fcc1616a2ec89904e9f1487a33ee0564a3efea
Tyrell C <tyrellrcurry@gmail.com>
2022-05-03 00:22:25 -0700

    Removed unused components, and added title & favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
-
 import Layout from '../components/Layout';
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({Component, pageProps}) {
- 
-  return(
+  return (
+    <div>
+      <Head>
+        <title>Entertainment App | In Demand Movies & TV Series</title>
+        <link rel="shortcut icon" href="./assets/favicon.ico" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-  ) 
+    </div>
+  );
 }
 
 export default MyApp;
